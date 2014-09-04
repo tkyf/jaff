7f4579a6ede63ad52eff775a5ccfb1de7fa8c0bd
tkyf <tkyf@users.noreply.github.com>
2014-09-04 23:55:05 +0900

    Add main options.

--- jaff.py
+++ jaff.py
@@ -2,8 +2,16 @@
 # coding:utf-8
 
 def main():
+
     pass
 
 if __name__ == "__main__":
+    import optparse
+    parser = optparse.OptionParser(description="Get word diff with POS.")
+    parser.add_option("-s", "--stats", dest="s", action="store_true", help="Calculate word diff statistics").
+    parser.add_option("-a", "--all", dest="a", action="store_true", help="    Output all texts").
+
+    (opts, args) = parser.parse_args()
+
     main()
 
