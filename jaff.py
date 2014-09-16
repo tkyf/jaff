6ab4747633ae365567d7f00c1ec1a715a3436ce8
tkyf <tkyf@users.noreply.github.com>
2014-09-16 23:17:00 +0900

    Add naive usage.

--- jaff.py
+++ jaff.py
@@ -2,16 +2,27 @@
 # coding:utf-8
 
 def main():
+    import argparse
+    import sys
 
-    pass
+    import align.align
+
+    #TODO argparseで引数を処理
+    parser = argparse.ArgumentParser(description="Get word diff with POS.")
+    parser.add_argument("-s", "--stats", dest="s", action="store_true", help="Calculate word diff statistics")
+    parser.add_argument("-a", "--all", dest="a", action="store_true", help="    Output all texts")
+    args = parser.parse_args()
+
+    if len(sys.argv) != 3:
+        print("Usage: $ python jaff textfile1 textfile2")
+        return 1
+    
+    base = sys.argv[1]
+    base = sys.argv[2]
+
+    return 0
 
 if __name__ == "__main__":
-    import optparse
-    parser = optparse.OptionParser(description="Get word diff with POS.")
-    parser.add_option("-s", "--stats", dest="s", action="store_true", help="Calculate word diff statistics")
-    parser.add_option("-a", "--all", dest="a", action="store_true", help="    Output all texts")
-
-    (opts, args) = parser.parse_args()
-
-    main()
+    import sys
+    sys.exit(main())
 
