cce7e28fe2f8eaf9ed5d695566262ddb312071d8
tkyf <tkyf@users.noreply.github.com>
2014-09-16 23:50:02 +0900

    Remove no using argpase.

--- jaff.py
+++ jaff.py
@@ -2,23 +2,19 @@
 # coding:utf-8
 
 def main():
-    import argparse
     import sys
 
     import align.align
-
-    #TODO argparseで引数を処理
-    parser = argparse.ArgumentParser(description="Get word diff with POS.")
-    parser.add_argument("-s", "--stats", dest="s", action="store_true", help="Calculate word diff statistics")
-    parser.add_argument("-a", "--all", dest="a", action="store_true", help="    Output all texts")
-    args = parser.parse_args()
 
     if len(sys.argv) != 3:
         print("Usage: $ python jaff textfile1 textfile2")
         return 1
     
     base = sys.argv[1]
-    base = sys.argv[2]
+    target = sys.argv[2]
+
+    alignment = align.align(base, target)
+    print(alignment)
 
     return 0
 
