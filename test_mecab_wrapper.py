bdb7722a7069bc216d6109a1d4dda9f1a3528772
tkyf <tkyf@users.noreply.github.com>
2014-11-18 23:43:24 +0900

    Add a test case.

--- test_mecab_wrapper.py
+++ test_mecab_wrapper.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 # -*- coding: utf-8 -*-
 
 import unittest
@@ -8,10 +9,13 @@ class MeCabWrpperTest(unittest.TestCase):
     
     def setUp(self):
         self.inputs = ['こんにちは']
+        self.answers= [[{'surface': 'こ', 'position': 'I', 'pos': '感動詞'}, {'surface': 'ん', 'position': 'I', 'pos': '感動詞'}, {'surface': 'に', 'position': 'I', 'pos': '感動詞'}, {'surface': 'ち', 'position': 'I', 'pos': '感動詞'}, {'surface': 'は', 'position': 'E', 'pos': '感動詞'}]
+                ]
 
     def test_tagging(self):
-        for string in self.inputs:
-            mecab_wrapper.tagging(string)
+        for in_str, answer in zip(self.inputs, self.answers):
+            self.assertEqual(mecab_wrapper.tagging(in_str), answer)
+            print(mecab_wrapper.tagging(in_str))
 
 
 if __name__ == '__main__':
