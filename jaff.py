5a1d6027b43e09dfd33b3b55100dbdb055e195be
tkyf <tkyf@users.noreply.github.com>
2014-11-21 00:22:14 +0900

    Add function of reflection word alignment to pos

--- jaff.py
+++ jaff.py
@@ -9,16 +9,23 @@ def jaff(str1, str2):
     m = mecab_wrapper.tagging(str1)
     print(a)
     print(m)
-    pos_align(a, m)
-    print(m)
-    return ""
+    pos_alignment = pos_align(a, m)
+    print(pos_alignment)
+    return (a[0], a[1], pos_alignment)
 
 def pos_align(alignment, tags, blank="\t"):
     for i, c in enumerate(alignment[0]):
         if c == blank:
             tags.insert(i, None)
 
+    pos_alignment = ''
+    for tag in tags:
+        if tag is None:
+            pos_alignment += blank
+        elif  tag['position'] == 'E':
+            pos_alignment += tag['pos']
 
+    return pos_alignment
 
 def main():
     import sys
